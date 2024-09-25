1ff040e00a46a586049aecde78bff28aa0dce5bd
harshit <harshitpareek893@gmail.com>
2024-09-25 13:21:42 +0530

    e

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -2,47 +2,48 @@ import dbConnect from "@/lib/dbconnection";
 import userModel from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 
-
-
-export async function POST(request : Request){
-   await  dbConnect();
-   const reqBody = await request.json();
-    const { username,  verifycode } = reqBody;
+export async function POST(request: NextRequest) {
+    await dbConnect();
     
-    const decodedusername = decodeURIComponent(username);
-    const user = await userModel.findOne({username : decodedusername});
+    const reqBody = await request.json();
+    const { username, verifycode } = reqBody;
 
-    if(!user){
-        return {statusCode: 404, body: JSON.stringify({message: "User not found"})}
-    }
-    if(user.isVerified){
+    const decodedusername = decodeURIComponent(username);
+    const user = await userModel.findOne({ username: decodedusername });
+
+    if (!user) {
         return NextResponse.json({
-            status : 200,
-            message: "User already verified"
-        })
+            status: 404,
+            message: "User not found",
+        }, { status: 404 });
+    }
+
+    if (user.isVerified) {
+        return NextResponse.json({
+            status: 200,
+            message: "User already verified",
+        });
     }
 
     const isVerificationCodeValid = (verifycode === user.verifyCode);
     const isVerificationCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
 
-    if(isVerificationCodeValid  && isVerificationCodeNotExpired){
-        user.isVerified = true ; 
+    if (isVerificationCodeValid && isVerificationCodeNotExpired) {
+        user.isVerified = true; 
         await user.save();
         return NextResponse.json({
-            status : 200,
-            message: "Verification successful"
-        })
-    }
-    else if(!isVerificationCodeNotExpired){
-      return NextResponse.json({
-            status : 403,
-            message: "Verification code expired"
-        })
-    }
-    else {
+            status: 200,
+            message: "Verification successful",
+        });
+    } else if (!isVerificationCodeNotExpired) {
         return NextResponse.json({
-            status : 402,
-            message: "Invalid Verification code"
-        })
+            status: 403,
+            message: "Verification code expired",
+        });
+    } else {
+        return NextResponse.json({
+            status: 402,
+            message: "Invalid Verification code",
+        });
     }
 }
